f0d5bc741dffe051eb827c1d116b0aa2d796a74a
JB-Gamelab <132678018+JB-Gamelab@users.noreply.github.com>
2023-05-16 22:44:53 +0100

    Section 1 finished

---
+++ RPG Project/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement {
+    public class Mover : MonoBehaviour {
+
+        private const string FORWARD_SPEED = "forwardSpeed";
+
+        private void Update() {
+            UpdateAnimator();
+        }
+
+
+        public void MoveTo(Vector3 destination) {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+
+        private void UpdateAnimator() {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat(FORWARD_SPEED, speed);
+        }
+    }
+}
