74419d27d2c159ce5f960e506110283be5930ac9
JB-Gamelab <132678018+JB-Gamelab@users.noreply.github.com>
2023-06-07 22:49:15 +0100

    Basic combat movement

--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -2,24 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using System;
+using RPG.Combat;
 
 namespace RPG.Control {
     public class PlayerController : MonoBehaviour {
 
         private void Update() {
-            if (Input.GetMouseButton(0)) {
-                MoveToCursor();
-            }
+
+            //If combat target, ignore skip movement logic
+            if (InteractWithCombat()) return;
+            if (InteractWithMovement()) return;
         }
 
-        private void MoveToCursor() {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        private bool InteractWithCombat() {
+
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            //Check if raycast hits a combat target
+            foreach (RaycastHit hit in hits) {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue; //if not a combat target, skip following logic and check next array entry
+
+                if (Input.GetMouseButtonDown(0)) {
+                    GetComponent<Fighter>().Attack(target);
+                }
+
+                return true;
+            }
+            return false;
+        }
+
+        private bool InteractWithMovement() {
+
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
             if (hasHit) {
-                GetComponent<Mover>().MoveTo(hit.point);
+                if (Input.GetMouseButton(0)) {
+                    GetComponent<Mover>().StartMoveAction(hit.point);
+                }
+                return true;
             }
+            return false;
+        }
+
+        //Raycast from camera to mouse cursor on Navmesh
+        private static Ray GetMouseRay() {
+
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
--- RPG Project/Assets/Scripts/Movement/Mover.cs
+++ RPG Project/Assets/Scripts/Movement/Mover.cs
@@ -1,3 +1,4 @@
+using RPG.Combat;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +9,33 @@ namespace RPG.Movement {
 
         private const string FORWARD_SPEED = "forwardSpeed";
 
+        private NavMeshAgent navMeshAgent;
+
+        private void Start() {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
         private void Update() {
             UpdateAnimator();
         }
 
+        public void StartMoveAction(Vector3 destination) {
+            GetComponent<Fighter>().Cancel();
+            MoveTo(destination);
+        }
+
 
         public void MoveTo(Vector3 destination) {
-            GetComponent<NavMeshAgent>().destination = destination;
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Stop() {
+            navMeshAgent.isStopped = true;
         }
 
         private void UpdateAnimator() {
-            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 velocity = navMeshAgent.velocity;
             Vector3 localVelocity = transform.InverseTransformDirection(velocity);
             float speed = localVelocity.z;
             GetComponent<Animator>().SetFloat(FORWARD_SPEED, speed);
