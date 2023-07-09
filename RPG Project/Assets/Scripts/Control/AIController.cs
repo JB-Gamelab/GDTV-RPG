1641665382404b2aafe104b15b0b5c85621e1e85
JB-Gamelab <132678018+JB-Gamelab@users.noreply.github.com>
2023-07-09 22:25:39 +0100

    Camera stuff

--- RPG Project/Assets/Scripts/Control/AIController.cs
+++ RPG Project/Assets/Scripts/Control/AIController.cs
@@ -15,6 +15,9 @@ namespace RPG.Control {
         [SerializeField] private PatrolPath patrolPath;
         [SerializeField] private float waypointTolerance = 1f;
         [SerializeField] private float waypointDwellTime = 3f;
+        [Range(0,1)]
+        [SerializeField] private float patrolSpeedFraction = 0.2f;
+
 
         private Fighter fighter;
         private Health health;
@@ -71,7 +74,7 @@ namespace RPG.Control {
             }
         
             if (timeSinceArrivedAtWaypoint > waypointDwellTime) {
-                mover.StartMoveAction(nextPosition);
+                mover.StartMoveAction(nextPosition, patrolSpeedFraction);
             }
         }
 
